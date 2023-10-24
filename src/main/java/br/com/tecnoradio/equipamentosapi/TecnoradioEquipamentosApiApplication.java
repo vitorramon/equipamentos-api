4508e774ac3dd2475b741114767251353af413c1
vitorramon <rvramon16@gmail.com>
2023-10-24 17:36:09 -0400

    first commit

---
+++ src/main/java/br/com/tecnoradio/equipamentosapi/TecnoradioEquipamentosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tecnoradio.equipamentosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnoradioEquipamentosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnoradioEquipamentosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tecnoradio/equipamentosapi/TecnoradioEquipamentosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tecnoradio.equipamentosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnoradioEquipamentosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
